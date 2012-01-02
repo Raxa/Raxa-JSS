c1c175327c264ac5f1dbb208b19bf2a098a5a210
Saptarshi P <sunbiz@gmail.com>
2012-01-02 17:46:55 +0530

    registration code v1 moved from dropbox/Code/JSS Registration Module

---
+++ src/registration/formToWizard.js
@@ -0,0 +1,73 @@
+/* Created by jankoatwarpspeed.com */
+
+(function($) {
+	$.fn.formToWizard = function(options) {
+		options = $.extend({
+			submitButton : ""
+		}, options);
+
+		var element = this;
+
+		var steps = $(element).find("fieldset");
+		var count = steps.size();
+		var submmitButtonName = "#" + options.submitButton;
+		$(submmitButtonName).hide();
+
+		// 2
+		$(element).before("<ul id='steps'></ul>");
+
+		steps.each(function(i) {
+			$(this).wrap("<div id='step" + i + "'></div>");
+			$(this).append("<p id='step" + i + "commands'></p>");
+
+			// 2
+			var name = $(this).find("legend").html();
+			$("#steps").append("<li id='stepDesc" + i + "'>Step " + (i + 1) + "<span>" + name + "</span></li>");
+
+			if(i == 0) {
+				createNextButton(i);
+				selectStep(i);
+			} else if(i == count - 1) {
+				$("#step" + i).hide();
+				createPrevButton(i);
+			} else {
+				$("#step" + i).hide();
+				createPrevButton(i);
+				createNextButton(i);
+			}
+		});
+		function createPrevButton(i) {
+			var stepName = "step" + i;
+			$("#" + stepName + "commands").append("<a href='#' id='" + stepName + "Prev' class='prev'>< Back</a>");
+
+			$("#" + stepName + "Prev").bind("click", function(e) {
+				$("#" + stepName).hide();
+				$("#step" + (i - 1)).show();
+				$(submmitButtonName).hide();
+				selectStep(i - 1);
+			});
+		}
+
+		function createNextButton(i) {
+			var stepName = "step" + i;
+			$("#" + stepName + "commands").append("<a href='#' id='" + stepName + "Next' class='next'>Save & Continue</a>");
+
+			$("#" + stepName + "Next").bind("click", function(e) {
+				$("#" + stepName).hide();
+				$("#step" + (i + 1)).show();
+				if(i + 2 == count)
+					$(submmitButtonName).show();
+				selectStep(i + 1);
+			});
+		}
+
+		function selectStep(i) {
+			$("#steps li").removeClass("current");
+			$("#stepDesc" + (i - 1)).addClass("past");
+			$("#stepDesc" + (i + 1)).removeClass("past");
+			$("#stepDesc" + (i)).removeClass("past");
+			$("#stepDesc" + i).addClass("current");
+		}
+
+	}
+})(jQuery);
