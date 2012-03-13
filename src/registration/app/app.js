7ed4c842a0b43c2e3921193c774aa684a2c74edd
Nathan Leiby <nathanleiby@gmail.com>
2012-03-13 06:38:40 -0700

    fixing major break -- removed Search from loaded Views, since it was just being used in local development

--- src/registration/app/app.js
+++ src/registration/app/app.js
@@ -6,7 +6,7 @@ Ext.application({
 	name: 'RaxaEmr.Registration',
 	controllers: ['Main'],
 	views: ['Home', 'Register', 'Search', 'Bmi', 'PatientScreen1', 'PatientScreen2', 'Viewport'],
-    models: ['Search','Patient'], // TODO: Remove temp 'Search' model
+    models: ['Patient'], 
 
 	launch: function() {
 		Ext.create('RaxaEmr.Registration.view.Viewport');
