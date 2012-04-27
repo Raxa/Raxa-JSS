f3f6477ee1ae5091c046487aadd19d561a55dcff
Saptarshi Purkayastha <sunbiz@gmail.com>
2012-04-27 14:31:43 +0530

    RAXAJSS-98 - Jasmine tests page setup for util methods

---
+++ test/specs/app/util-specs.js
@@ -0,0 +1,20 @@
+/**
+ * This is the script for jasmine specs to test src/resources/scripts/util.js
+ */
+describe("Get Basic Auth Header", function() {
+    it("returns BasicAuthHeader", function() {
+        expect(Util.getBasicAuthHeader("admin","Hello123")).toEqual("Authorization: Basic YWRtaW46SGVsbG8xMjM=");
+    });
+});
+
+describe("Get JSON Accept Type", function() {
+    it("returns AcceptType", function() {
+        expect(Util.getJsonAcceptType()).toEqual("Accept: application/json");
+    });
+});
+
+describe("Get JSON Content-Type", function() {
+    it("returns Content-Type", function() {
+        expect(Util.getJsonContentType()).toEqual("Content-Type: application/json");
+    });
+});
