a16a78d0b5c09f0ac063cc364da8a0bfc256c46c
John Stoecker <johnstoecker@gmail.com>
2012-05-21 14:13:10 +0530

    RAXAJSS-111 adding inList and notInList for PatientList service layer

--- api/src/main/java/org/raxa/module/raxacore/PatientListService.java
+++ api/src/main/java/org/raxa/module/raxacore/PatientListService.java
@@ -3,17 +3,14 @@ package org.raxa.module.raxacore;
 /**
  * Copyright 2012, Raxa
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 import java.util.List;
 import org.openmrs.Encounter;
@@ -28,91 +25,91 @@ import org.raxa.module.raxacore.db.PatientListDAO;
  * Interface for interacting with the PatientList
  */
 public interface PatientListService extends OpenmrsService {
-	
-	/**
-	 * Sets Patient List DAO
-	 *
-	 * @param dao
-	 */
-	public void setPatientListDAO(PatientListDAO dao);
-	
-	/**
-	 * Saves PatientList
-	 *
-	 * @param patientList
-	 * @return PatientList
-	 */
-	@Authorized( { "Add Patient Lists" })
-	public PatientList savePatientList(PatientList patientList);
-	
-	/**
-	 * Gets a PatientList by Id
-	 *
-	 * @param id
-	 * @return PatientLists
-	 */
-	@Authorized( { "View Patient Lists" })
-	public PatientList getPatientList(Integer id);
-	
-	/**
-	 * Gets a PatientList by Name
-	 *
-	 * @param name
-	 * @return list of PatientLists
-	 */
-	@Authorized( { "View Patient Lists" })
-	public List<PatientList> getPatientListByName(String name);
-	
-	/**
-	 * Gets PatientList by uuid
-	 *
-	 * @param uuid
-	 * @return PatientList
-	 */
-	@Authorized( { "View Patient Lists" })
-	public PatientList getPatientListByUuid(String uuid);
-	
-	/**
-	 * Gets PatientLists by EncounterType
-	 *
-	 * @param encounterType
-	 * @return list of PatientLists
-	 */
-	@Authorized(value = { "View Patient Lists", PrivilegeConstants.VIEW_ENCOUNTER_TYPES }, requireAll = true)
-	public List<PatientList> getPatientListByEncounterType(EncounterType encounterType);
-	
-	/**
-	 * Gets all patients in PatientList
-	 *
-	 * @param patientList
-	 * @return list of Patients
-	 */
-	@Authorized(value = { "View Patient Lists", PrivilegeConstants.VIEW_PATIENTS }, requireAll = true)
-	public List<Patient> getPatientsInPatientList(PatientList patientList);
-	
-	/**
-	 * Gets all the encounters in PatientList
-	 * 
-	 * @param patientList
-	 * @return list of Encounters
-	 */
-	@Authorized(value = { "View Patient Lists", PrivilegeConstants.VIEW_ENCOUNTERS }, requireAll = true)
-	public List<Encounter> getEncountersInPatientList(PatientList patientList);
-	
-	/**
-	 * Updates PatientList
-	 *
-	 * @param patientList
-	 * @return PatientList
-	 */
-	@Authorized( { "Edit Patient Lists" })
-	PatientList updatePatientList(PatientList patientList);
-	
-	/**
-	 * Deletes PatientList
-	 *
-	 * @param patientList
-	 */
-	@Authorized( { "Delete Patient Lists" })
-	public void deletePatientList(PatientList patientList);
+
+    /**
+     * Sets Patient List DAO
+     *
+     * @param dao
+     */
+    public void setPatientListDAO(PatientListDAO dao);
+
+    /**
+     * Saves PatientList
+     *
+     * @param patientList
+     * @return PatientList
+     */
+    @Authorized( {"Add Patient Lists"})
+    public PatientList savePatientList(PatientList patientList);
+
+    /**
+     * Gets a PatientList by Id
+     *
+     * @param id
+     * @return PatientLists
+     */
+    @Authorized( {"View Patient Lists"})
+    public PatientList getPatientList(Integer id);
+
+    /**
+     * Gets a PatientList by Name
+     *
+     * @param name
+     * @return list of PatientLists
+     */
+    @Authorized( {"View Patient Lists"})
+    public List<PatientList> getPatientListByName(String name);
+
+    /**
+     * Gets PatientList by uuid
+     *
+     * @param uuid
+     * @return PatientList
+     */
+    @Authorized( {"View Patient Lists"})
+    public PatientList getPatientListByUuid(String uuid);
+
+    /**
+     * Gets PatientLists by EncounterType
+     *
+     * @param encounterType
+     * @return list of PatientLists
+     */
+    @Authorized(value = {"View Patient Lists", PrivilegeConstants.VIEW_ENCOUNTER_TYPES}, requireAll = true)
+    public List<PatientList> getPatientListByEncounterType(EncounterType encounterType);
+
+    /**
+     * Gets all patients in PatientList
+     *
+     * @param patientList
+     * @return list of Patients
+     */
+    @Authorized(value = {"View Patient Lists", PrivilegeConstants.VIEW_PATIENTS}, requireAll = true)
+    public List<Patient> getPatientsInPatientList(PatientList patientList);
+
+    /**
+     * Gets all the encounters in PatientList
+     *
+     * @param patientList
+     * @return list of Encounters
+     */
+    @Authorized(value = {"View Patient Lists", PrivilegeConstants.VIEW_ENCOUNTERS}, requireAll = true)
+    public List<Encounter> getEncountersInPatientList(PatientList patientList);
+
+    /**
+     * Updates PatientList
+     *
+     * @param patientList
+     * @return PatientList
+     */
+    @Authorized( {"Edit Patient Lists"})
+    PatientList updatePatientList(PatientList patientList);
+
+    /**
+     * Deletes PatientList
+     *
+     * @param patientList
+     */
+    @Authorized( {"Delete Patient Lists"})
+    public void deletePatientList(PatientList patientList);
 }
--- api/src/main/java/org/raxa/module/raxacore/impl/PatientListServiceImpl.java
+++ api/src/main/java/org/raxa/module/raxacore/impl/PatientListServiceImpl.java
@@ -16,6 +16,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -33,146 +34,180 @@ import org.raxa.module.raxacore.db.PatientListDAO;
  * "?encounterType=<uuid>&startDate=2012-05-07&endDate=2012-05-08&inlist=<uuidForList>&notinlist=<uuidForList>"
  */
 public class PatientListServiceImpl implements PatientListService {
-	
-	private PatientListDAO dao;
-	
-	private Log log = LogFactory.getLog(this.getClass());
-	
-	/**
-	 * @see org.raxa.module.raxacore.PatientListService#setPatientListDAO
-	 */
-	@Override
-	public void setPatientListDAO(PatientListDAO dao) {
-		this.dao = dao;
-	}
-	
-	/**
-	 * @see org.raxa.module.raxacore.PatientListService#savePatientList
-	 */
-	@Override
-	public PatientList savePatientList(PatientList patientList) {
-		return dao.savePatientList(patientList);
-	}
-	
-	/**
-	 * @see org.raxa.module.raxacore.PatientListService#getPatientList(Integer)
-	 */
-	@Override
-	public PatientList getPatientList(Integer id) {
-		return dao.getPatientList(id);
-	}
-	
-	/**
-	 * @see org.raxa.module.raxacore.PatientListService#getPatientListByName(String)
-	 */
-	@Override
-	public List<PatientList> getPatientListByName(String name) {
-		return dao.getPatientListByName(name);
-	}
-	
-	/**
-	 * @see org.raxa.module.raxacore.PatientListService#getPatientListByUuid(String)
-	 */
-	@Override
-	public PatientList getPatientListByUuid(String uuid) {
-		return dao.getPatientListByUuid(uuid);
-	}
-	
-	/**
-	 * @see org.raxa.module.raxacore.PatientListService#getPatientListByEncounterType
-	 */
-	@Override
-	public List<PatientList> getPatientListByEncounterType(EncounterType encounterType) {
-		return dao.getPatientListByEncounterType(encounterType);
-	}
-	
-	/**
-	 * Parses a string into a date
-	 * @param str String to be parsed (must be iso format)
-	 * @return Date 
-	 */
-	private Date getDateFromString(String str) {
-		
-		String[] supportedFormats = { "yyyy-MM-dd'T'HH:mm:ss.SSSZ", "yyyy-MM-dd'T'HH:mm:ss.SSS", "yyyy-MM-dd'T'HH:mm:ssZ",
-		        "yyyy-MM-dd'T'HH:mm:ss", "yyyy-MM-dd HH:mm:ss", "yyyy-MM-dd" };
-		for (int i = 0; i < supportedFormats.length; i++) {
-			try {
-				Date date = new SimpleDateFormat(supportedFormats[i]).parse(str);
-				return date;
-			}
-			catch (ParseException ex) {}
-		}
-		log.error("Error parsing string " + str + " into Date");
-		return null;
-	}
-	
-	/**
-	 * @see org.raxa.module.raxacore.PatientListService#getEncountersInPatientList
-	 */
-	@Override
-	public List<Encounter> getEncountersInPatientList(PatientList patientList) {
-		String query = patientList.getSearchQuery();
-		//"?encounterType=<uuid>&startDate=2012-05-07&endDate=2012-05-08&inlist=<uuidForList>&notinlist=<uuidForList>"
-		EncounterType encType = null;
-		Date startDate = null;
-		Date endDate = null;
-		String[] queryFields = query.split("&");
-		//if we have an encountertype in our search query, set it
-		for (Integer i = 0; i < queryFields.length; i++) {
-			if (queryFields[i].indexOf("?encounterType=") != -1) {
-				encType = Context.getEncounterService().getEncounterTypeByUuid(queryFields[i].substring(15));
-			} else if (queryFields[i].indexOf("startDate=") != -1) {
-				startDate = getDateFromString(queryFields[i].substring(10));
-			} else if (queryFields[i].indexOf("endDate=") != -1) {
-				endDate = getDateFromString(queryFields[i].substring(8));
-			}
-		}
-		
-		List<EncounterType> encTypes = new ArrayList<EncounterType>();
-		encTypes.add(encType);
-		return Context.getEncounterService().getEncounters(null, null, startDate, endDate, null, encTypes, null,
-		    Boolean.FALSE);
-	}
-	
-	/**
-	 * @see org.raxa.module.raxacore.PatientListService#getPatientsInList
-	 */
-	@Override
-	public List<Patient> getPatientsInPatientList(PatientList patientList) {
-		List<Encounter> encounters = getEncountersInPatientList(patientList);
-		List<Patient> patients = new ArrayList<Patient>();
-		for (Integer j = 0; j < encounters.size(); j++) {
-			//just in case we have an encounter for the same type with the same patient, check if already exists
-			if (!patients.contains(encounters.get(j).getPatient())) {
-				patients.add(encounters.get(j).getPatient());
-			}
-		}
-		return patients;
-	}
-	
-	/**
-	 * @see org.raxa.module.raxacore.PatientListService#updatePatientList
-	 */
-	@Override
-	public PatientList updatePatientList(PatientList patientList) {
-		return dao.updatePatientList(patientList);
-	}
-	
-	/**
-	 * @see org.raxa.module.raxacore.PatientListService#deletePatientList
-	 */
-	@Override
-	public void deletePatientList(PatientList patientList) {
-		dao.deletePatientList(patientList);
-	}
-	
-	@Override
-	public void onStartup() {
-		System.out.print("Starting patient list service");
-	}
-	
-	@Override
-	public void onShutdown() {
-		System.out.print("Stopping patient list service");
-	}
+
+    private PatientListDAO dao;
+    private Log log = LogFactory.getLog(this.getClass());
+
+    /**
+     * @see org.raxa.module.raxacore.PatientListService#setPatientListDAO
+     */
+    @Override
+    public void setPatientListDAO(PatientListDAO dao) {
+        this.dao = dao;
+    }
+
+    /**
+     * @see org.raxa.module.raxacore.PatientListService#savePatientList
+     */
+    @Override
+    public PatientList savePatientList(PatientList patientList) {
+        return dao.savePatientList(patientList);
+    }
+
+    /**
+     * @see org.raxa.module.raxacore.PatientListService#getPatientList(Integer)
+     */
+    @Override
+    public PatientList getPatientList(Integer id) {
+        return dao.getPatientList(id);
+    }
+
+    /**
+     * @see org.raxa.module.raxacore.PatientListService#getPatientListByName(String)
+     */
+    @Override
+    public List<PatientList> getPatientListByName(String name) {
+        return dao.getPatientListByName(name);
+    }
+
+    /**
+     * @see org.raxa.module.raxacore.PatientListService#getPatientListByUuid(String)
+     */
+    @Override
+    public PatientList getPatientListByUuid(String uuid) {
+        return dao.getPatientListByUuid(uuid);
+    }
+
+    /**
+     * @see org.raxa.module.raxacore.PatientListService#getPatientListByEncounterType
+     */
+    @Override
+    public List<PatientList> getPatientListByEncounterType(EncounterType encounterType) {
+        return dao.getPatientListByEncounterType(encounterType);
+    }
+
+    /**
+     * Parses a string into a date
+     *
+     * @param str String to be parsed (must be iso format)
+     * @return Date
+     */
+    private Date getDateFromString(String str) {
+
+        String[] supportedFormats = {"yyyy-MM-dd'T'HH:mm:ss.SSSZ", "yyyy-MM-dd'T'HH:mm:ss.SSS", "yyyy-MM-dd'T'HH:mm:ssZ",
+            "yyyy-MM-dd'T'HH:mm:ss", "yyyy-MM-dd HH:mm:ss", "yyyy-MM-dd"};
+        for (int i = 0; i < supportedFormats.length; i++) {
+            try {
+                Date date = new SimpleDateFormat(supportedFormats[i]).parse(str);
+                return date;
+            } catch (ParseException ex) {
+            }
+        }
+        log.error("Error parsing string " + str + " into Date");
+        return null;
+    }
+
+    /**
+     * @see org.raxa.module.raxacore.PatientListService#getEncountersInPatientList
+     */
+    @Override
+    public List<Encounter> getEncountersInPatientList(PatientList patientList) {
+        if (patientList.getSearchQuery() == null) {
+            return null;
+        }
+        String query = patientList.getSearchQuery();
+        if (query.indexOf("?") == 0) {
+            query = query.substring(1);
+        }
+        //"?encounterType=<uuid>&startDate=2012-05-07&endDate=2012-05-08&inlist=<uuidForList>&notinlist=<uuidForList>"
+        EncounterType encType = null;
+        Date startDate = null;
+        Date endDate = null;
+        //the return value can only choose encounters from this list (if not null)
+        List<Encounter> inListEncounters = null;
+        //the return value can not contain any encounters from this list
+        List<Encounter> notInListEncounters = new ArrayList<Encounter>();
+        String[] queryFields = query.split("&");
+        //if we have an encountertype in our search query, set it
+        for (int i = 0; i < queryFields.length; i++) {
+            if (queryFields[i].indexOf("encounterType=") != -1) {
+                encType = Context.getEncounterService().getEncounterTypeByUuid(queryFields[i].substring(14));
+            } else if (queryFields[i].indexOf("startDate=") != -1) {
+                startDate = getDateFromString(queryFields[i].substring(10));
+            } else if (queryFields[i].indexOf("endDate=") != -1) {
+                endDate = getDateFromString(queryFields[i].substring(8));
+            } else if (queryFields[i].indexOf("inList=") != -1) {
+                inListEncounters = new ArrayList<Encounter>();
+                //could be multiple lists here, add all that are in
+                String[] inListUuids = queryFields[i].substring(7).split(",");
+                for (int j = 0; j < inListUuids.length; j++) {
+                    inListEncounters.addAll(getEncountersInPatientList(getPatientListByUuid(inListUuids[j])));
+                }
+            } else if (queryFields[i].indexOf("notInList=") != -1) {
+                String[] notInListUuids = queryFields[i].substring(10).split(",");
+                for (int k = 0; k < notInListUuids.length; k++) {
+                    notInListEncounters.addAll(getEncountersInPatientList(getPatientListByUuid(notInListUuids[k])));
+                }
+            }
+        }
+
+        List<EncounterType> encTypes = new ArrayList<EncounterType>();
+        encTypes.add(encType);
+        List<Encounter> encs = Context.getEncounterService().getEncounters(null, null, startDate, endDate, null, encTypes,
+                null, Boolean.FALSE);
+        encs.removeAll(notInListEncounters);
+        if (inListEncounters != null) {
+            Iterator<Encounter> iter = encs.iterator();
+            //if encounter is not in inListEncounters, remove it
+            while (iter.hasNext()) {
+                Encounter currEnc = iter.next();
+                if (!inListEncounters.contains(currEnc)) {
+                    iter.remove();
+                }
+            }
+        }
+        return encs;
+    }
+
+    /**
+     * @see org.raxa.module.raxacore.PatientListService#getPatientsInList
+     */
+    @Override
+    public List<Patient> getPatientsInPatientList(PatientList patientList) {
+        List<Encounter> encounters = getEncountersInPatientList(patientList);
+        List<Patient> patients = new ArrayList<Patient>();
+        for (Integer j = 0; j < encounters.size(); j++) {
+            //just in case we have an encounter for the same type with the same patient, check if already exists
+            if (!patients.contains(encounters.get(j).getPatient())) {
+                patients.add(encounters.get(j).getPatient());
+            }
+        }
+        return patients;
+    }
+
+    /**
+     * @see org.raxa.module.raxacore.PatientListService#updatePatientList
+     */
+    @Override
+    public PatientList updatePatientList(PatientList patientList) {
+        return dao.updatePatientList(patientList);
+    }
+
+    /**
+     * @see org.raxa.module.raxacore.PatientListService#deletePatientList
+     */
+    @Override
+    public void deletePatientList(PatientList patientList) {
+        dao.deletePatientList(patientList);
+    }
+
+    @Override
+    public void onStartup() {
+        System.out.print("Starting patient list service");
+    }
+
+    @Override
+    public void onShutdown() {
+        System.out.print("Stopping patient list service");
+    }
 }
