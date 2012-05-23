f3bf2c3ac09214cfc2ff33af09749dc50de2cdf3
John Stoecker <johnstoecker@gmail.com>
2012-05-23 11:53:26 +0530

    RAXAJSS-111 refactoring JUnit tests so each test checks only one part of a function

--- api/src/main/java/org/raxa/module/raxacore/PatientListService.java
+++ api/src/main/java/org/raxa/module/raxacore/PatientListService.java
@@ -25,91 +25,91 @@ import org.raxa.module.raxacore.db.PatientListDAO;
  * Interface for interacting with the PatientList
  */
 public interface PatientListService extends OpenmrsService {
-
-    /**
-     * Sets Patient List DAO
-     *
-     * @param dao
-     */
-    public void setPatientListDAO(PatientListDAO dao);
-
-    /**
-     * Saves PatientList
-     *
-     * @param patientList
-     * @return PatientList
-     */
-    @Authorized( {"Add Patient Lists"})
-    public PatientList savePatientList(PatientList patientList);
-
-    /**
-     * Gets a PatientList by Id
-     *
-     * @param id
-     * @return PatientLists
-     */
-    @Authorized( {"View Patient Lists"})
-    public PatientList getPatientList(Integer id);
-
-    /**
-     * Gets a PatientList by Name
-     *
-     * @param name
-     * @return list of PatientLists
-     */
-    @Authorized( {"View Patient Lists"})
-    public List<PatientList> getPatientListByName(String name);
-
-    /**
-     * Gets PatientList by uuid
-     *
-     * @param uuid
-     * @return PatientList
-     */
-    @Authorized( {"View Patient Lists"})
-    public PatientList getPatientListByUuid(String uuid);
-
-    /**
-     * Gets PatientLists by EncounterType
-     *
-     * @param encounterType
-     * @return list of PatientLists
-     */
-    @Authorized(value = {"View Patient Lists", PrivilegeConstants.VIEW_ENCOUNTER_TYPES}, requireAll = true)
-    public List<PatientList> getPatientListByEncounterType(EncounterType encounterType);
-
-    /**
-     * Gets all patients in PatientList
-     *
-     * @param patientList
-     * @return list of Patients
-     */
-    @Authorized(value = {"View Patient Lists", PrivilegeConstants.VIEW_PATIENTS}, requireAll = true)
-    public List<Patient> getPatientsInPatientList(PatientList patientList);
-
-    /**
-     * Gets all the encounters in PatientList
-     *
-     * @param patientList
-     * @return list of Encounters
-     */
-    @Authorized(value = {"View Patient Lists", PrivilegeConstants.VIEW_ENCOUNTERS}, requireAll = true)
-    public List<Encounter> getEncountersInPatientList(PatientList patientList);
-
-    /**
-     * Updates PatientList
-     *
-     * @param patientList
-     * @return PatientList
-     */
-    @Authorized( {"Edit Patient Lists"})
-    PatientList updatePatientList(PatientList patientList);
-
-    /**
-     * Deletes PatientList
-     *
-     * @param patientList
-     */
-    @Authorized( {"Delete Patient Lists"})
-    public void deletePatientList(PatientList patientList);
+	
+	/**
+	 * Sets Patient List DAO
+	 *
+	 * @param dao
+	 */
+	public void setPatientListDAO(PatientListDAO dao);
+	
+	/**
+	 * Saves PatientList
+	 *
+	 * @param patientList
+	 * @return PatientList
+	 */
+	@Authorized( { "Add Patient Lists" })
+	public PatientList savePatientList(PatientList patientList);
+	
+	/**
+	 * Gets a PatientList by Id
+	 *
+	 * @param id
+	 * @return PatientLists
+	 */
+	@Authorized( { "View Patient Lists" })
+	public PatientList getPatientList(Integer id);
+	
+	/**
+	 * Gets a PatientList by Name
+	 *
+	 * @param name
+	 * @return list of PatientLists
+	 */
+	@Authorized( { "View Patient Lists" })
+	public List<PatientList> getPatientListByName(String name);
+	
+	/**
+	 * Gets PatientList by uuid
+	 *
+	 * @param uuid
+	 * @return PatientList
+	 */
+	@Authorized( { "View Patient Lists" })
+	public PatientList getPatientListByUuid(String uuid);
+	
+	/**
+	 * Gets PatientLists by EncounterType
+	 *
+	 * @param encounterType
+	 * @return list of PatientLists
+	 */
+	@Authorized(value = { "View Patient Lists", PrivilegeConstants.VIEW_ENCOUNTER_TYPES }, requireAll = true)
+	public List<PatientList> getPatientListByEncounterType(EncounterType encounterType);
+	
+	/**
+	 * Gets all patients in PatientList
+	 *
+	 * @param patientList
+	 * @return list of Patients
+	 */
+	@Authorized(value = { "View Patient Lists", PrivilegeConstants.VIEW_PATIENTS }, requireAll = true)
+	public List<Patient> getPatientsInPatientList(PatientList patientList);
+	
+	/**
+	 * Gets all the encounters in PatientList
+	 *
+	 * @param patientList
+	 * @return list of Encounters
+	 */
+	@Authorized(value = { "View Patient Lists", PrivilegeConstants.VIEW_ENCOUNTERS }, requireAll = true)
+	public List<Encounter> getEncountersInPatientList(PatientList patientList);
+	
+	/**
+	 * Updates PatientList
+	 *
+	 * @param patientList
+	 * @return PatientList
+	 */
+	@Authorized( { "Edit Patient Lists" })
+	PatientList updatePatientList(PatientList patientList);
+	
+	/**
+	 * Deletes PatientList
+	 *
+	 * @param patientList
+	 */
+	@Authorized( { "Delete Patient Lists" })
+	public void deletePatientList(PatientList patientList);
 }
