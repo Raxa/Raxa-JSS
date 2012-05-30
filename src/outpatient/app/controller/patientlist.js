0ab7a7b85b4cfd54cb2db64fc6ba791897577ba8
vikas singh <vikassingh266@gmail.com>
2012-05-30 20:07:12 +0530

    RAXAJSS-168 - Added ability to Filter and Sort the Patient List

--- src/outpatient/app/controller/patientlist.js
+++ src/outpatient/app/controller/patientlist.js
@@ -5,6 +5,11 @@ Ext.define('RaxaEmr.Outpatient.controller.patientlist', {
         refs: {
             main: 'mainview',
             contacts: 'patientlist',
+            contact: '#contact',
+            name: '#name',
+            docname: '#docname',
+            urgency: '#urgency',
+            lastvisit: '#lastvisit',
             showContact: 'patientlist-show'
         },
 
@@ -15,10 +20,33 @@ Ext.define('RaxaEmr.Outpatient.controller.patientlist', {
             },
             contacts: {
                 itemtap: 'onContactSelect'
+            },
+            name: {
+                tap: 'sortbyname'
+            },
+            docname: {
+                tap: 'sortbydocname'
+            },
+            urgency: {
+                tap: 'sortbyurgency'
+            },
+            lastvisit: {
+                tap: 'sortbylastvisit'
+            },
+            searchfield: {
+                clearicontap: 'onSearchClearIconTap',
+                keyup: 'onSearchKeyUp'
             }
         }
     },
 
+    onMainPush: function (view, item) {
+
+        if (item.xtype == "contact-show") {
+            this.getContacts().deselectAll();
+        }
+
+    },
 
     onContactSelect: function (list, index, node, record) {
 
@@ -29,4 +57,66 @@ Ext.define('RaxaEmr.Outpatient.controller.patientlist', {
         this.showContact.setRecord(record);
         this.getMain().push(this.showContact);
     },
+    sortbyname: function () {
+        store = this.getContact().getStore();
+        store.setSorters("firstName");
+        store.load();
+    },
+
+    sortbydocname: function () {
+        store = this.getContact().getStore();
+        store.setSorters("nameofdoc");
+        store.load();
+    },
+
+    sortbyurgency: function () {
+        store = this.getContact().getStore();
+        store.setSorters("urgency");
+        store.load();
+    },
+    sortbylastvisit: function () {
+        store = this.getContact().getStore();
+        store.setSorters("lastvisit");
+        store.load();
+    },
+
+    onSearchKeyUp: function (field) {
+
+        var value = field.getValue(),
+            store = this.getContact().getStore();
+
+        store.clearFilter();
+
+        if (value) {
+            var searches = value.split(' '),
+                regexps = [],
+                i;
+
+            for (i = 0; i < searches.length; i++) {
+                if (!searches[i]) continue;
+                regexps.push(new RegExp(searches[i], 'i'));
+            }
+
+            store.filter(function (record) {
+                var matched = [];
+
+                for (i = 0; i < regexps.length; i++) {
+                    var search = regexps[i],
+                        didMatch = record.get('firstName').match(search);
+                    matched.push(didMatch);
+                }
+
+                if (regexps.length > 1 && matched.indexOf(false) != -1) {
+                    return false;
+                } else {
+                    return matched[0];
+                }
+            });
+        }
+    },
+
+    onSearchClearIconTap: function () {
+        store = this.getContact().getStore();
+        store.clearFilter();
+    }
 });
