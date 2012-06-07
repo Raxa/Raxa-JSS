077d8f07bda7b1eccc963db2168f6e7ccfa7c17c
tushar bansal <bansal.tushar92@gmail.com>
2012-06-07 15:34:57 +0530

    RAXAJSS-155 Screener: Write Model for Doctor based on mocked JSON active doctor list

---
+++ test/specs/screener/DoctorListSpec.js
@@ -0,0 +1,21 @@
+describe("DoctorList", function () {
+    var store = null;
+    beforeEach(function () {
+        if (!store) {
+            store = Ext.create('Screener.store.DoctorList');
+        }
+        expect(store).toBeTruthy()
+        waitsFor(
+            function () {
+                return !store.isLoading();
+            }, "load never completed", 4000)
+    });
+  
+    it("saves in localstorage", function () {
+        expect(store.getCount()).toEqual(2);
+        expect(store.getById(0).data.uuid).toEqual("b0763c23-95e7-11e1-beba-4dc2e8449b3e");
+        expect(store.getById(1).data.uuid).toEqual("050128e0-08ee-4290-a1e0-2455a1518e50");
+        expect(store.getById(0).data.display).toEqual("admin - Super User");
+        expect(store.getById(1).data.display).toEqual("007 - Saptarshi Purkayastha");
+    });
+});
