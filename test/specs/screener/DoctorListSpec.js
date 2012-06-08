d927ae0589e5bc2466eaa1abb345839f7d24269f
tushar bansal <bansal.tushar92@gmail.com>
2012-06-08 12:50:20 +0530

    RAXAJSS-155 Screener: Write Model for Doctor based on mocked JSON active doctor list

--- test/specs/screener/DoctorListSpec.js
+++ test/specs/screener/DoctorListSpec.js
@@ -1,5 +1,6 @@
 describe("DoctorList", function () {
     var store = null;
+    var timeout = 4000;
     beforeEach(function () {
         if (!store) {
             store = Ext.create('Screener.store.DoctorList');
@@ -8,10 +9,10 @@ describe("DoctorList", function () {
         waitsFor(
             function () {
                 return !store.isLoading();
-            }, "load never completed", 4000)
+            }, "load never completed", timeout)
     });
   
-    it("saves in localstorage", function () {
+    it("reading from DoctorList store & comparing with json file", function () {
         expect(store.getCount()).toEqual(2);
         expect(store.getById(0).data.uuid).toEqual("b0763c23-95e7-11e1-beba-4dc2e8449b3e");
         expect(store.getById(1).data.uuid).toEqual("050128e0-08ee-4290-a1e0-2455a1518e50");
