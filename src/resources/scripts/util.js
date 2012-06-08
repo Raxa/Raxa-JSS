ae22769d088e99b34f05a62953091bd7d1988de1
Tarang <tarangmahajan@gmail.com>
2012-06-08 17:33:41 +0530

    RAXAJSS-183 Function Added in util.js to share Models between ExtJS and Touch

--- src/resources/scripts/util.js
+++ src/resources/scripts/util.js
@@ -109,3 +109,20 @@ var Util = {
         return deviceId;
     }
 }
+
+//Function to help share Models between ExtJS and Sencha Touch 2.
+var platformizeModelConfig = function(extJsModelConfig) {
+    if(Ext.versions.extjs) {
+        return extJsModelConfig; // nothing to change, we are on ext
+    } else if(Ext.versions.touch) {
+        // transform to Sencha Touch 2 data model
+        var config = {
+            extend: extJsModelConfig.extend,
+            config: extJsModelConfig
+        };
+        delete config.config.extend;
+        return config;
+    } else {
+        Ext.Error.raise('Could not recognize Library');
+    }
+};
