eca16e252b4e4b496d1360fabb2c39a138840343
leifdejong <leifdejong@gmail.com>
2012-06-27 15:35:46 -0400

    Moved drugSubmitButton changes to the controller and tested

--- src/screener/app/view/PharmacyForm.js
+++ src/screener/app/view/PharmacyForm.js
@@ -6,7 +6,7 @@
 Ext.define("Screener.view.PharmacyForm", {
     xtype: 'pharmacyForm',
     extend: 'Ext.form.Panel',
-    requires: ['Ext.tab.Panel', 'Ext.form.FieldSet', 'Ext.MessageBox'],
+    requires: ['Ext.tab.Panel', 'Ext.form.FieldSet'],
     config: {
         styleHtmlContent: true,
         xtype: 'orderform',
@@ -53,22 +53,7 @@ Ext.define("Screener.view.PharmacyForm", {
                 id: 'drugSubmitButton',
                 height: '40px',
                 text: 'submit',
-                width: '200px',
-                handler: function () {
-                	// changes the button text to 'Confirm' and 'Cancel'
-                	var MB = Ext.MessageBox;
-                	Ext.apply(MB, {
-	                	YES: { text: 'Confirm', itemId: 'yes', ui: 'action' },
-	                	NO: { text: 'Cancel', itemId: 'no' }
-	                });
-	                	Ext.apply(MB, {
-		                	YESNO: [MB.NO, MB.YES]
-		                });
-		            // on-click, launch MessageBox
-		            Ext.get('drugSubmitButton').on('click', function(e){
-		              Ext.Msg.confirm("Confirmation", "Are you sure you want to submit your Pharmacy Order?", Ext.emptyFn);
-	                });
-                }
+                width: '200px'
             }]
         }]
     }
