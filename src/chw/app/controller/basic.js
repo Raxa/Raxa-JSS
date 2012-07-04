80a842e2d6b896e67ddc86a3b69a395bf9c2d818
Joy <jming@college.harvard.edu>
2012-07-04 14:30:28 +0530

    functionality to ok/cancel

--- src/chw/app/controller/basic.js
+++ src/chw/app/controller/basic.js
@@ -339,12 +339,17 @@ Ext.define('mUserStories.controller.basic', {
     // distinguish between ok and cancel
     doOption: function (arg) {
         var active = Ext.getCmp('viewPort').getActiveItem().getActiveItem();
-        // console.log(active);
-        if (arg) {
-            if (active === 0) {
-                this.doLogin(true);
-            }
-        } else {
+        if (active === PAGES.LOGIN_SCREEN) {
+            this.doLogin(arg)
+        } else if (active === PAGES.ADD_REG) {
+            this.doAdd('reg',arg)
+        } else if (active === PAGES.ADD_REM) {
+            this.doAdd('rem',arg)
+        } else if (active === PAGES.ADD_APP) {
+            this.doAdd('app',arg)
+        } else if (active === PAGES.INBOX_CHW) {
+            
+        } else if (active === PAGES.INBOX_VC) {
             
         }
     },
