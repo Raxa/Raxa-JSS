c43f1a96eae8377ac7152a0fdc178d26b6173ead
Joy <jming@college.harvard.edu>
2012-07-04 13:55:21 +0530

    changing urls

--- src/resources/scripts/util.js
+++ src/resources/scripts/util.js
@@ -160,7 +160,7 @@ var Util = {
      */
     getModules: function () {
         //always keep login at first position as its app path is different
-        return ['login', 'screener', 'registration', 'registrationextjs4', 'pharmacy', 'chw'];
+        return ['login', 'screener', 'registration', 'registrationextjs4', 'pharmacy', 'chw', 'outpatient'];
         //TO DO:Add the line below instead the above one 
         //return ['login', 'screener', 'registration','opd','inpatient','pharmacy','radiology','laboratory','billing'];
     },
