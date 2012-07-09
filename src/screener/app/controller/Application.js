f10860de29e7176c5221db635328d150ffed1460
Tarang <tarangmahajan@gmail.com>
2012-07-09 18:01:33 +0530

    Code optimized

--- src/screener/app/controller/Application.js
+++ src/screener/app/controller/Application.js
@@ -401,16 +401,10 @@ Ext.define("Screener.controller.Application", {
         store.load({
             callback: function (records, operation, success) {
                 // the operation object contains all of the details of the load operation
-                Ext.getCmp('1').setHtml("");
-                Ext.getCmp('2').setHtml("");
-                Ext.getCmp('3').setHtml("");
-                Ext.getCmp('4').setHtml("");
-                Ext.getCmp('5').setHtml("");
-                Ext.getCmp('1').setHtml(store.last().raw.obs[0].display);
-                Ext.getCmp('2').setHtml(store.last().raw.obs[1].display);
-                Ext.getCmp('3').setHtml(store.last().raw.obs[2].display);
-                Ext.getCmp('4').setHtml(store.last().raw.obs[3].display);
-                Ext.getCmp('5').setHtml(store.last().raw.obs[4].display);
+                for(i=1;i<=5;i++) {
+                Ext.getCmp('i').setHtml("");
+                Ext.getCmp('5').setHtml(store.last().raw.obs[i-1].display);
+                }
             },
             scope: this
         });
