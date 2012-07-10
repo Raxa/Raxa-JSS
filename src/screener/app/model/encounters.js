1b6fe8eb491a23fc86bc2800c688f74c57d7a280
tushar bansal <bansal.tushar92@gmail.com>
2012-07-10 16:50:54 +0530

    added chw.png and encounters.js fixed git case error 2

---
+++ src/screener/app/model/encounters.js
@@ -0,0 +1,24 @@
+Ext.define('Screener.model.Encounters', {
+    extend: 'Ext.data.Model',
+    config: {
+        fields: [{
+            name: 'uuid',
+            type: 'string'
+        }, {
+            name: 'display',
+            type: 'string'
+        }, {
+            name: 'encounterType',
+            type: 'string'
+        }, {
+            name: 'encounterDatetime',
+            type: 'string'
+        }, {
+            name: 'provider',
+            type: 'string'
+        }, {
+            name: 'obs',
+            model: 'Screener.model.Observation'
+        }]
+    }
+});
