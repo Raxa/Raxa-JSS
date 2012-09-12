fadf57424a462f8df0feec01509f3ec22a086cc0
computer <computer@computer-PC>
2012-09-12 10:56:10 +0530

    Unique id for patient created . RAXAJSS-379

--- src/screener/app/view/Main.js
+++ src/screener/app/view/Main.js
@@ -9,33 +9,20 @@ Ext.define("Screener.view.Main", {
     extend: 'Ext.Container',
     xtype: 'mainView',
     initialize: function (args) {
-        var topBar = Ext.create('Topbar.view.TopToolbar', {
-});
+        var topBar = Ext.create('Topbar.view.TopToolbar');
         this.add(topBar);
         Ext.getCmp('topbarSelectfield').setValue("Screener");
-       // topBar.getElementById('topbarSelectfield').setValue("Screener");
-      // var value = Ext.getCmp('topbarSelectfield').getValue();
-       //console.log(value);
-       //value = "Screener";
-       //console.log(value);
-     //  value.setData("Screener");
-        // topBar.initialize.value = "Screener";    
-        //console.log(Ext.getCmp('topbarSelectfield').getValue()); 
     },
     config: {
         fullscreen: true,
-
         //don't delete views so we can switch screens quickly
         autoDestroy: false,
-
         items: [{
             title: "JSS Hospital Screener System",
             items: [
             {
-                    
                 xtype: 'topmenu'
             },
-            
             ]
         }, ]
     }
