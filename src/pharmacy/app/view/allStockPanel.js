5da69e1c39298cb7357880deda00ffc8c6805ee4
Nathan Leiby <nathanleiby@gmail.com>
2012-10-28 11:53:41 +0530

    RAXAJSS-615 remove nav buttons from inventory view

--- src/pharmacy/app/view/allStockPanel.js
+++ src/pharmacy/app/view/allStockPanel.js
@@ -3,39 +3,34 @@ Ext.define('RaxaEmr.Pharmacy.view.allStockPanel', {
     alias: 'widget.allStockPanel',
     id: 'allStockPanel',
     border: false,
-    height: 100,
     width: 840,
     layout: {
         type: 'hbox'
     },
-    margin: '20 0 0 110',
+    margin: '10 0 10 110',
     items: [
     {
         xtype: 'button',
         id: 'availableStockButton',
-        width: 150,
-        height: 80,
+        margin: '0 10 0 0',
         text: 'Available Stock',
         action: 'showAvailableStock'
     },{
         xtype: 'button',
         id: 'expiringStockButton',
-        width: 150,
-        height: 80,
+        margin: '0 10 0 0',
         text: 'Expiring Stock',
         action: 'showExpiringStock'
     },{
         xtype: 'button',
         id: 'lowStockButton',
-        width: 150,
-        height: 80,
+        margin: '0 10 0 0',
         text: 'Low Stock',
         action: 'showStockOut'
     },{
         xtype: 'button',
         id: 'allOrdersButton',
-        width: 150,
-        height: 80,
+        margin: '0 10 0 0',
         text: 'All Orders',
         action: 'showAllOrders'
     }
