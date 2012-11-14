50a9e8ef4c479b7ae5eb91fa667b276ab22e9816
Nathan Leiby <nathanleiby@gmail.com>
2012-11-14 18:06:15 +0530

    RAXAJSS-623 remove extra files from temp modalGrid project

--- src/modalGrid/app/store/RequisitionItems.js
+++
@@ -1,9 +0,0 @@
-Ext.define('RaxaEmr.Pharmacy.store.RequisitionItems', {
-    extend: 'Ext.data.Store',
-    fields: ['drugname', 'quantity'],
-    autoLoad: true,
-    autoSync: false,
-    proxy: {
-        type: 'localstorage'
-    }
-});
--- src/modalGrid/app/store/drugOrderPatient.js
+++
@@ -1,5 +0,0 @@
-/* store for searching(GET calls) patients in advanced search */
-Ext.define('RaxaEmr.Pharmacy.store.drugOrderPatient',{
-    extend: 'Ext.data.Store',
-    model: 'RaxaEmr.Pharmacy.model.drugOrderPatient'
-})
