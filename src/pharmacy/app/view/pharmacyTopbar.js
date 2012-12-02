d3c410a76c45e34efa2ead8701ce6d21284114cb
naomi <enduser@blackbox.twinklethecat.co.uk>
2012-12-02 18:52:21 +0000

    Indentation

--- src/pharmacy/app/view/pharmacyTopbar.js
+++ src/pharmacy/app/view/pharmacyTopbar.js
@@ -27,7 +27,11 @@ Ext.define('RaxaEmr.Pharmacy.view.pharmacyTopbar',{
 	    src:  '../resources/img/icon_s.png',
             width: PHARMACY_TOPBAR_CONSTANTS.BUTTON_HEIGHT,
 	    listeners: {
-	        afterrender: function(c) { c.el.on("click", function() { window.location = '../#Dashboard'; })}
+	        afterrender: function(c) {
+	            c.el.on("click", function() {
+			 window.location = '../#Dashboard'; 
+		    })
+		}
 	    }
         }, {
             xtype: 'button',
