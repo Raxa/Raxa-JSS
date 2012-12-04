4de2588f17d51dbd3bb5d1d65c27ccf0f6becb6f
madanpiyush <madanpiyush@gmail.com>
2012-12-04 12:26:07 +0530

    i18n is removed from Login screen

--- src/app/app.js
+++ src/app/app.js
@@ -6,16 +6,6 @@ Ext.Loader.setConfig({
     }
 });
 
-//i18n
-Ext.require('Ext.i18n.Bundle', function () {
-    Ext.i18n.appBundle = Ext.create('Ext.i18n.Bundle', {
-        bundle: 'RaxaEmr',
-        //Specify language here.
-        lang: 'en-US',
-        path: 'app/view', // Path to the .properties file
-        noCache: true
-    });
-});
 
 // Main application entry point
 Ext.application({
